e63323e4e4bab079f9e7313fb780c208c1e9c008
Thomas Wong <38015348+ThomasWongMingHei@users.noreply.github.com>
2018-10-21 13:20:13 +0100

    u

--- fitOU.R
+++ fitOU.R
@@ -7,7 +7,7 @@ alldata$time <- strptime(x = as.character(alldata$time),
                                 format = "%Y-%m-%d %H:%M:%S")
 alldata<-na.omit(alldata)
 mid<-(alldata['bid']+alldata['ask'])/2
-start<-500000
+start<-580000
 mid<-mid[start:699842,]
 df_ts <- ts(data=mid,start=start,end=699842,frequency = 1)
 
@@ -29,15 +29,15 @@ ou.coe
 
 ou.sim <- function(a,b,c,N,startval){
   return(sde.sim(0,1,startval,N,
-                  drift = expression(1.00000000-0.46957458*x),
+                  drift = expression(1.00000000-0.46489810*x),
                   sigma = expression(0.01776727),sigma.x = expression(0)))
   }
 
 output<-matrix(0, 5064, 2000)
 for (i in 1:600){
 walk<-sde.sim(0,1,2.172,101280,
-        drift = expression(1-0.46302879*x),
-        sigma = expression(0.01214844),sigma.x = expression(0))
+        drift = expression(1-0.46489810*x),
+        sigma = expression(0.01438023 ),sigma.x = expression(0))
 output[,i]<-walk[seq(2,101281,20)]
 }
 write.csv(output, file = "D:/Algooutput2.csv")
